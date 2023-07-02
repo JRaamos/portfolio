816eb3b2aed979e739bdc370d16f048f38e5e80a
JRaamos <jhonyramos46@gmail.com>
2023-07-02 17:13:20 -0300

    adicionar stylo a barra de navegação lateral

--- src/components/NavLateral.js
+++ src/components/NavLateral.js
@@ -2,27 +2,41 @@ import React from 'react';
 import github from '../img/github.webp';
 import instagran from '../img/instagran.png';
 import linkedin from '../img/linkedin.webp';
+import './NavLateral.css';
 
 function NavLateral() {
   return (
-    <div>
+    <div className="nav-lateral">
       <nav>
         <a
           href="https://github.com/JRaamos"
           target="_blank"
           rel="noreferrer"
         >
-          <img src={ github } alt="github" />
+          <img
+            src={ github }
+            alt="github"
+            className="img-nav"
+          />
         </a>
         <a
           href="https://www.instagram.com/jonathan_febraio/"
           target="_blank"
           rel="noreferrer"
+
         >
-          <img src={ instagran } alt="instagran" />
+          <img
+            src={ instagran }
+            alt="instagran"
+            className="img-nav"
+          />
         </a>
         <a href="https://www.linkedin.com/in/jonathan-febraio-dev/" target="_blank" rel="noreferrer">
-          <img src={ linkedin } alt="linkedin" />
+          <img
+            src={ linkedin }
+            alt="linkedin"
+            className="img-nav"
+          />
         </a>
       </nav>
     </div>
