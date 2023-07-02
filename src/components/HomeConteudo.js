e1b0c45da3be944bafc3a7bbccacc1dc46605eae
JRaamos <jhonyramos46@gmail.com>
2023-07-02 20:29:00 -0300

    adicionar stylo a main da pagina home

--- src/components/HomeConteudo.js
+++ src/components/HomeConteudo.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import minhaFoto from '../img/eu.jpeg';
+import './HomeConteudo.css';
 
 function HomeConteudo() {
   return (
     <div
-      className="home-conteudo"
+      className="home-contain"
     >
-      <h1>
-        Olá, eu sou o Jonathan Febraio
-      </h1>
-      <p>
-        Sou um Desenvolverdor Fronte-End que escreve codigos limpo, elgante e eficiente,
-        apaixonado por tecnologia, sempre em busca de novos conhecimentos e desafios.
-      </p>
-      <section>
-        <img src={ minhaFoto } alt="Minha foto" />
-      </section>
+      <div className="home-conteudo">
+        <section className="home-text">
+          <h1 className="text-h1">
+            Olá, eu sou o Jonathan Febraio
+          </h1>
+          <p className="text-p">
+            Sou um Desenvolverdor Fronte-End que escreve codigos
+            limpo, elgante e eficiente,
+            apaixonado por tecnologia, sempre em busca de novos conhecimentos e desafios.
+          </p>
+        </section>
+        <section>
+          <img
+            className="minha-foto"
+            src={ minhaFoto }
+            alt="Minha foto"
+          />
+        </section>
+      </div>
     </div>
   );
 }
