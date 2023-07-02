e240978f1eb76c2f11010c6087de10ca815e6f5b
JRaamos <jhonyramos46@gmail.com>
2023-07-02 15:57:57 -0300

    adiciona style inicial ao header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { handleGravatar } from '../helpers/featFunctions';
+import './Header.css';
 
 function Header() {
   return (
-    <div>
-      <img src={ handleGravatar('jhonyramos46@gmail.com') } alt="avatar" />
-      <h2>Jonathan Febraio</h2>
-      <h3>HOME</h3>
-      <h3>ABOUT</h3>
-      <h3>PROJECTS</h3>
-      <h3>CONTACT</h3>
+    <div className="header-contain">
+      <div className="avata">
+        <img
+          className="img-gravatar"
+          src={ handleGravatar('jhonyramos46@gmail.com') }
+          alt="avatar"
+        />
+        <h2>Jonathan Febraio</h2>
+      </div>
+      <nav className="navegacao">
+        <h3 className="header-navegacao">Home</h3>
+        <h3 className="header-navegacao">About</h3>
+        <h3 className="header-navegacao">Project</h3>
+        <h3 className="header-navegacao">Contact</h3>
+      </nav>
     </div>
   );
 }
