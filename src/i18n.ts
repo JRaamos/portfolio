a02b05d437f227c32181bfcfc742d6e0a4ee02ed
JRaamos <jhonyramos46@gmail.com>
2023-12-23 12:45:25 -0300

    fix: ajuste de diretorio

--- src/i18n.ts
+++ src/i18n.ts
@@ -15,7 +15,7 @@ i18n
       caches: ['cookie'],
     },
     backend: {
-      loadPath: '/src/locales/{{lng}}/translation.json',
+      loadPath: '../locales/{{lng}}/translation.json',
     },
     interpolation: {
       escapeValue: false,
