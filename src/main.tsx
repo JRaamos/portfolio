8aa77d80c4f208bbd2e1eb504207382dc8b85f62
JRaamos <jhonyramos46@gmail.com>
2023-12-23 12:57:49 -0300

    fix: ajuste de arquivos

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
-import './i18n';
+import '../i18n.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
