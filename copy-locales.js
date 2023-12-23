0e7a185b86b2e7ffb52d219a87f440ecaf179a03
JRaamos <jhonyramos46@gmail.com>
2023-12-23 13:28:01 -0300

    feat: criação da pasta public

--- copy-locales.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs-extra');
-
-fs.copy('locales', 'dist/locales', function (err) {
-  if (err) {
-    console.error('Error copying locales:', err);
-  } else {
-    console.log('Locales copied successfully.');
-  }
-});
