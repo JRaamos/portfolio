0448f7a358b83c8dee3727d428042bb5351bb4ea
JRaamos <jhonyramos46@gmail.com>
2023-12-26 22:38:24 -0300

    fix: ajuste de posição de imenssagem para responsividade

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -17,39 +17,39 @@ function About() {
       <section
         className="flex flex-col justify-center items-center w-2/3"
       >
-        <div>
+        <div className=" my-4">
           <p className="text-lg italic font-semibold  w-full">{t('aboutTextInto')}</p>
           <p className=" w-full font-mono">{t('aboutText')}</p>
         </div>
-        <div>
+        <div className=" my-4">
           <p className="text-lg italic font-semibold  w-full">{t('aboutCareerInto')}</p>
           <p className=" w-full font-mono">{t('aboutTextCarrer')}</p>
         </div>
-        <div>
+        <div className=" my-4">
           <p className="text-lg italic font-semibold  w-full">
             {t('aboutExperienceInto')}
           </p>
           <p className=" w-full font-mono">{t('aboutTextExperience')}</p>
         </div>
-        <div>
+        <div className=" my-4">
           <p className="text-lg italic font-semibold  w-full">
             {t('aboutObjectivesInto')}
           </p>
           <p className=" w-full font-mono">{t('aboutTextObjectives')}</p>
         </div>
-        <div>
+        <div className="my-4">
           <p className="text-lg italic font-semibold  w-full">
             {t('aboutcuriosityInto')}
           </p>
           <p className=" w-full font-mono">{t('aboutTextCuriosity')}</p>
         </div>
         <div
-          className="flex w-full justify-center items-center mt-10
+          className="flex w-full justify-center items-center mt-6
        mb-16 max-md:flex-col"
         >
           <p className="font-mono text-lg italic ">{t('conect')}</p>
           <button
-            className="mx-2  hadow-4xl bg-amber-400
+            className=" w-96 hadow-4xl bg-amber-400
              dark:text-slate-900 p-1 rounded-lg h-12"
           >
             {t('button')}
