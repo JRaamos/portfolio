903be4dd8006b739d2ba68c5c6293855395a6767
JRaamos <jhonyramos46@gmail.com>
2024-01-05 18:24:22 -0300

    feat: adicona responsividade a todo o projeto

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -18,20 +18,29 @@ function ProjectCard({ projectId, title, img, linkSite, github }:
     <div
       className="w-full mt-6 p-5 flex"
     >
-      <div className="flex max-lg:flex-col w-full justify-around">
+      <div
+        className="flex max-lg:flex-col w-full justify-around
+      max-lg:justify-center max-lg:items-center"
+      >
         <img
           src={ img }
           alt={ title }
           className="w-[60vh] h-[40vh]
-          rounded-2xl m-4"
+          rounded-2xl m-4  max-lg:w-[80%] max-lg:h-[60%]"
         />
-        <div className="flex flex-col w-[60vh] m-10">
+        <div className="flex flex-col w-[60vh] m-10 max-lg:w-2/3 max-lg:items-center">
           <h1
-            className="text-2xl font-semibold mb-5"
+            className="text-2xl font-semibold mb-5 max-lg:text-center"
           >
             {title}
           </h1>
-          <p className="font-mono text-xl">{description}</p>
+          <p
+            className="font-mono text-xl max-lg:text-center
+           max-lg:w-full"
+          >
+            {description}
+
+          </p>
           <Link to={ `/project/${projectId}` } className="w-36">
             <button
               className="bg-indigo-700 text-white p-1 rounded-md w-36 mt-5
